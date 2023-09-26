e10e01c5f061bab232d90cb0890ee1aeba1a5457
gyurison7 <orangetree401@gmail.com>
2023-09-26 22:48:15 +0900

    feat: TypeScript forms 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setUsername(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello", username);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="black"/>
-      <Circle bgColor="tomato" text="I'm here!"/>
+      <form onSubmit={onSubmit}>
+        <input
+          value={username}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Login</button>
+      </form>
     </div>
   );
 }
