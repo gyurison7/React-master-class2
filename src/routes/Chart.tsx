e1506aa8717ca07c219482f6951c6935c9bee7f5
gyurison7 <orangetree401@gmail.com>
2023-09-30 04:50:08 +0900

    feat: crypto tracker

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,78 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexCharts from "react-apexcharts";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+function Chart() {
+  const { coinId } = useParams();
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId || "")
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <ApexCharts
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) || [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+            },
+            xaxis: {
+              labels: { show: false },
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              type: "datetime",
+              categories: data?.map(price => price.time_close)
+            },
+            yaxis: {
+              show: false,
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(2)}`
+              }
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chart;
