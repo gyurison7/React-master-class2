b27085bf0bb5356406470170780c6a72c5500f92
gyurison7 <orangetree401@gmail.com>
2023-10-02 01:11:35 +0900

    fix: 차트 테마 컬러 적용 수정

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,7 +1,8 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
 import ApexCharts from "react-apexcharts";
-import { useTheme } from "styled-components";
+import { useRecoilValue } from "recoil";
+import { themeState } from "../atom";
 
 interface ChartProps {
   coinId?: string;
@@ -22,7 +23,7 @@ export function Chart({ coinId }: ChartProps) {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId || "")
   );
-  const theme = useTheme();
+  const themeMode = useRecoilValue(themeState);
 
   return (
     <div>
@@ -53,22 +54,10 @@ export function Chart({ coinId }: ChartProps) {
               },
             },
             theme: {
-              mode: "dark",
+              mode: themeMode === "light" ? "light" : "dark",
             },
             xaxis: {
               type: "datetime",
-              labels: {
-                style: {
-                  colors: Array(7).fill(theme.textColor),
-                },
-              },
-            },
-            yaxis: {
-              labels: {
-                style: {
-                  colors: Array(7).fill(theme.textColor),
-                },
-              },
             },
           }}
         />
